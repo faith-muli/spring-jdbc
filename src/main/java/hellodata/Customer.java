0ad7df536c4947eefc1f1dc95bde4037ac57312f
faith-muli <faithmuli33@gmail.com>
2016-08-22 12:22:48 +0300

    spring with jdbc

---
+++ src/main/java/hellodata/Customer.java
@@ -0,0 +1,22 @@
+package hellodata;
+
+/**
+ * Created by faith on 8/22/16.
+ */
+public class Customer {
+
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+
+    }
+    @Override
+    public String toString(){
+        return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName,lastName);
+    }
+
+}
